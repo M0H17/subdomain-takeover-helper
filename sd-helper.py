af99af025ab8c0dc2a8e51fc2630dab1dd8cf500
M0H17 <mohit.v@provensec.com>
2018-08-25 21:47:34 +0530

    Fixed common errors

--- sd-helper.py
+++ sd-helper.py
@@ -2,48 +2,67 @@ import requests
 import dns.resolver
 
 domain = "domain.com"
-apikey = "NULL" 
+apikey = "NULL"
+
+def banner():
+    # print("##     ##  #######  ##     ##    ##   ########\n###   ### ##     ## ##     ##  ####   ##    ##\n#### #### ##     ## ##     ##    ##       ##\n## ### ## ##     ## #########    ##      ##\n##     ## ##     ## ##     ##    ##     ##\n##     ## ##     ## ##     ##    ##     ##\n##     ##  #######  ##     ##  ######   ##\n")
+    print("Author: M0H17")
+
+banner()
 
 def total():
     url="https://www.virustotal.com/vtapi/v2/domain/report?apikey="+ apikey + "&domain=" + domain
-    resp = requests.get(url)
-    resp = resp.json()
-    subdomains = resp["subdomains"]
-    file = open("vt_subdomains" , "w+")
-    for subdomain in subdomains:
-        file.write(subdomain + "\n")
+    try:
+        resp = requests.get(url)
+        resp = resp.json()
+        subdomains = resp["subdomains"]
+        file = open("vt_subdomains" , "w+")
+        print("Extracting Subdomains from Virustotal")
+        for subdomain in subdomains:
+            file.write(subdomain + "\n")
+    except Exception as e:
+        print("Exception: " + str(e))
 
 def dnss(sub):
     out = ""
-    for test in dns.resolver.query(sub,"CNAME"):
-        out = test.target
-    return out
+    try:
+        for test in dns.resolver.query(sub,"CNAME"):
+            out = test.target
+        return out
+    except Exception as e:
+        return e
 
 def storage1():
+    print("Checking VirusTotal Subdomains")
     total()
     file = open("vt_subdomains", "r")
+    print("~"*160)
     for subdomain in file.readlines():
         subdomain=subdomain.rstrip('\n\r')
-        r = requests.get('http://'+subdomain)
-        protocol = r.url.split('://')[0]
         try:
-            res = requests.get(str(protocol) + "://" + subdomain)
+            r = requests.get('http://'+subdomain)
+            protocol = r.url.split('://')[0]
             try:
-                out = dnss(subdomain)
-                print(
-                    str(res.status_code) + "\t\t" + subdomain + "\t\t" + str(res.headers.get("server")) + "\t\t" + str(out))
-            except Exception as exc:
-                if ("CNAME" in str(exc)):
-                    print(str(res.status_code) + "\t\t" + subdomain + "\t\t" + str(res.headers.get("server")) + "\t\t" + str(exc))
-                    continue
+                res = requests.get(str(protocol) + "://" + subdomain)
+                try:
+                    out = dnss(subdomain)
+                    print("Host:\t" + subdomain + "\n" + "Status:\t" + str(res.status_code) + "\n" + "Server Version:\t" + res.headers.get("server")+ "\n" + "CNAME:\t" + str(out))
+                    print("~" * 160)
+                except Exception as exc:
+                    if ("CNAME" in str(exc)):
+                        print("Host:\t" + subdomain + "\n" + "Status:\t" + str(res.status_code) + "\n" + "Server Version:\t" + res.headers.get("server") + "\n" + "CNAME:\t" + str(exc))
+                        print("~" * 160)
+                        continue
+            except Exception as e:
+                print("exception here" + str(e))
+                continue
         except Exception as e:
-            print("exception here" + str(e))
             continue
+    print("Listed all Sub-Domains found on VirusTotal")
     print("\nYou can check results upto till done scanning process in the vt_subdomains.txt file")
 
 def storage2():
     check = input("Do you want to check subdomains using wordlist (Y/N)")
-    print("Status" + "\t\t" + "Host" + "\t\t\t\t\t\t\t" + "Server Version" + "\t\t\t\t" + "CNAME")
     check = str(check).lower()
     if(check == "y"):
         targets = open("wordlist.txt","r")
@@ -56,10 +75,12 @@ def storage2():
                 res = requests.get(subd)
                 try:
                     out = dnss(sub)
-                    print(str(res.status_code) + "\t\t\t" + subd + "\t\t" + str(res.headers.get("server")) + "\t\t" + str(out))
+                    print("Host:\t" + subd + "\n" + "Status:\t" + str(res.status_code) + "\n" + "Server Version:\t" + res.headers.get("server") + "\n" + "CNAME:\t" + str(out))
+                    print("~" * 160)
                 except Exception as exc:
                     if ("CNAME" in str(exc)):
-                        print(str(res.status_code) + "\t\t\t" + subd + "\t\t" + str(res.headers.get("server")) + "\t\t" + str(exc))
+                        print("Host:\t" + subd + "\n" + "Status:\t" + str(res.status_code) + "\n" + "Server Version:\t" + res.headers.get("server") + "\n" + "CNAME:\t" + str(exc))
+                        print("~" * 160)
                         continue
             except:
                 continue
