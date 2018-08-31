5986b126c4982815e0ec35a41b5766e4021c6af5
M0H17 <mohit.v@provensec.com>
2018-08-31 09:53:04 +0530

    structured, removed unused code

--- sd-helper.py
+++ sd-helper.py
@@ -1,12 +1,11 @@
 import requests
 import dns.resolver
 
-domain = "domain.com"
+domain = ""
 apikey = "NULL"
 
 def banner():
-    # print("##     ##  #######  ##     ##    ##   ########\n###   ### ##     ## ##     ##  ####   ##    ##\n#### #### ##     ## ##     ##    ##       ##\n## ### ## ##     ## #########    ##      ##\n##     ## ##     ## ##     ##    ##     ##\n##     ## ##     ## ##     ##    ##     ##\n##     ##  #######  ##     ##  ######   ##\n")
-    print("Author: M0H17")
+        print("Author: M0H17")
 
 banner()
 
@@ -23,67 +22,61 @@ def total():
     except Exception as e:
         print("Exception: " + str(e))
 
-def dnss(sub):
-    out = ""
+def cnames(sub):
+    cname = ""
     try:
         for test in dns.resolver.query(sub,"CNAME"):
-            out = test.target
-        return out
+            cname = test.target
+        return str(cname)
     except Exception as e:
-        return e
+        return str(e)
+
+def request_send(subdomain):
+    try:
+        r = requests.get('http://' + subdomain)
+        protocol = r.url.split('://')[0]
+        try:
+            res = requests.get(str(protocol) + "://" + subdomain)
+            try:
+                cname = cnames(subdomain)
+            except Exception as e1:
+                cname = str(e1)
+                pass
+            printer(res, cname)
+        except Exception as e:
+            print("Exception Occured While Sending Request 1: " + str(e))
+    except Exception as e:
+        if("Max retries exceeded with url" in str(e)):
+            pass
+        else:
+            print("Exception Occured While Sending Request 2: " + str(e))
 
 def storage1():
     print("Checking VirusTotal Subdomains")
     total()
     file = open("vt_subdomains", "r")
-    print("~"*160)
     for subdomain in file.readlines():
         subdomain=subdomain.rstrip('\n\r')
-        try:
-            r = requests.get('http://'+subdomain)
-            protocol = r.url.split('://')[0]
-            try:
-                res = requests.get(str(protocol) + "://" + subdomain)
-                try:
-                    out = dnss(subdomain)
-                    print("Host:\t" + subdomain + "\n" + "Status:\t" + str(res.status_code) + "\n" + "Server Version:\t" + res.headers.get("server")+ "\n" + "CNAME:\t" + str(out))
-                    print("~" * 160)
-                except Exception as exc:
-                    if ("CNAME" in str(exc)):
-                        print("Host:\t" + subdomain + "\n" + "Status:\t" + str(res.status_code) + "\n" + "Server Version:\t" + res.headers.get("server") + "\n" + "CNAME:\t" + str(exc))
-                        print("~" * 160)
-                        continue
-            except Exception as e:
-                print("exception here" + str(e))
-                continue
-        except Exception as e:
-            continue
-    print("Listed all Sub-Domains found on VirusTotal")
-    print("\nYou can check results upto till done scanning process in the vt_subdomains.txt file")
+        request_send(subdomain)
+    print("\nListed all Sub-Domains found on VirusTotal")
+    print("You can check results upto till done scanning process in the vt_subdomains.txt file")
+
+def printer(res,cname):
+    print("~"*30)
+    print("Host:\t" + str(res.url) + "\n" + "Status:\t" + str(
+        res.status_code) + "\t\t\t" + "Server Version:\t" + res.headers.get("server") + "\n" + "CNAME:\t" + str(
+        cname) + "\n")
 
 def storage2():
-    check = input("Do you want to check subdomains using wordlist (Y/N)")
+    check = input("\nDo you want to check subdomains using wordlist (Y/N)")
     check = str(check).lower()
     if(check == "y"):
         targets = open("wordlist.txt","r")
         content = targets.readlines()
         content = [x.strip() for x in content]
         for items in content:
-            sub = items + "." + domain
-            subd = "http://" + sub
-            try:
-                res = requests.get(subd)
-                try:
-                    out = dnss(sub)
-                    print("Host:\t" + subd + "\n" + "Status:\t" + str(res.status_code) + "\n" + "Server Version:\t" + res.headers.get("server") + "\n" + "CNAME:\t" + str(out))
-                    print("~" * 160)
-                except Exception as exc:
-                    if ("CNAME" in str(exc)):
-                        print("Host:\t" + subd + "\n" + "Status:\t" + str(res.status_code) + "\n" + "Server Version:\t" + res.headers.get("server") + "\n" + "CNAME:\t" + str(exc))
-                        print("~" * 160)
-                        continue
-            except:
-                continue
+            subomain = items + "." + domain
+            request_send(subomain)
 
 if(apikey != "NULL"):
     storage1()
@@ -91,4 +84,3 @@ if(apikey != "NULL"):
 else:
     print("You might have not provided the virustotal API-KEY in the script.")
     storage2()
-
