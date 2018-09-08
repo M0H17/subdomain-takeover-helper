37ff84d2856beaa8ae232d0e975a22e258657d61
M0H17 <mohit.v@provensec.com>
2018-09-08 12:40:42 +0530

    CNAME AUTHORITY RESPONSE FIXED

--- sd-helper.py
+++ sd-helper.py
@@ -1,5 +1,7 @@
 import requests
 import dns.resolver
+import subprocess
+import sys
 
 domain = "domain-here"
 apikey = "NULL"
@@ -8,7 +10,6 @@ def banner():
     # print("##     ##  #######  ##     ##    ##   ########\n###   ### ##     ## ##     ##  ####   ##    ##\n#### #### ##     ## ##     ##    ##       ##\n## ### ## ##     ## #########    ##      ##\n##     ## ##     ## ##     ##    ##     ##\n##     ## ##     ## ##     ##    ##     ##\n##     ##  #######  ##     ##  ######   ##\n")
     print("Author: M0H17")
 
-banner()
 
 def total():
     url="https://www.virustotal.com/vtapi/v2/domain/report?apikey="+ apikey + "&domain=" + domain
@@ -23,14 +24,26 @@ def total():
     except Exception as e:
         print("Exception: " + str(e))
 
-def cnames(sub):
-    cname = ""
-    try:
-        for test in dns.resolver.query(sub,"CNAME"):
-            cname = test.target
-        return str(cname)
-    except Exception as e:
-        return str(e)
+
+def cnames(ip):
+    cmd = "dig CNAME " + ip
+    name = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    out = name.communicate()
+    out = list(out).__str__().split(";;")
+    output = []
+    for lines in out:
+        if ("SECTION:" in str(lines)):
+            lines = str(lines).replace('\\n', '\\t').split("\\t")
+            for items in str(lines).split():
+                if (items.__len__() > 5):
+                    if ("CNAME" not in items):
+                        output.append(str(items) + " ")
+    for i in range(0, 3):
+        output.pop(0)
+    str1 = ''.join(output)
+    str1 = str1.replace("'", "").replace(";", " ").replace(",", "")
+    return str(str1)
+
 
 def request_send(subdomain):
     try:
@@ -53,6 +66,7 @@ def request_send(subdomain):
         else:
             print("Exception Occured While Sending Request 2: " + str(e))
 
+
 def storage1():
     print("Checking VirusTotal Subdomains")
     total()
@@ -63,6 +77,7 @@ def storage1():
     print("\nListed all Sub-Domains found on VirusTotal")
     print("You can check results upto till done scanning process in the vt_subdomains.txt file")
 
+
 def ipresolver(subdomain):
     try:
         ip = dns.resolver.query(subdomain)
@@ -71,22 +86,31 @@ def ipresolver(subdomain):
     except Exception as e:
         return str(e)
 
+
 def printer(res,cname,ip):
-    print("~"*30)
-    print("IP: " + str(ip) + "\t\tHost:\t" + str(res.url) + "\n" + "Status:\t" + str(
-        res.status_code) + "\t\t\t\t" + "Server Version:\t" + res.headers.get("server") + "\n" + "CNAME:\t" + str(
+    print("~"*70)
+    print("IP: " + str(ip) + "\t\tHost:\t" + str(res.url) + "\t\t" + "Status:\t" + str(
+        res.status_code) + "\t\t\t" + "Server Version:\t" + res.headers.get("server") + "\n" + "CNAME:\t" + str(
         cname) + "\n")
 
+
 def storage2():
-    check = input("\nDo you want to check subdomains using wordlist (Y/N)")
-    check = str(check).lower()
-    if(check == "y"):
-        targets = open("wordlist.txt","r")
-        content = targets.readlines()
-        content = [x.strip() for x in content]
-        for items in content:
-            subomain = items + "." + domain
-            request_send(subomain)
+    try: 
+    	check = raw_input("\nDo you want to check subdomains using wordlist? (Y/N) : ")
+    	check = str(check).lower()
+    	if(check == "y"):
+        	targets = open("wordlist.txt","r")
+        	content = targets.readlines()
+        	content = [x.strip() for x in content]
+        	for items in content:
+            		subomain = items + "." + domain
+            		request_send(subomain)
+    	else:
+		print("EXITING")
+    except Exception as e:
+		print("Exception Occured: " + str(e))
+
+
 
 if(apikey != "NULL"):
     storage1()
@@ -94,3 +118,4 @@ if(apikey != "NULL"):
 else:
     print("You might have not provided the virustotal API-KEY in the script.")
     storage2()
+
